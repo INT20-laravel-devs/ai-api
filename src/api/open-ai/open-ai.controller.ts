7d98691e4eb61cfe347fb74d87f98586e2024a14
Illia <kroshka.illya@lll.kpi.ua>
2025-03-01 18:22:01 +0200

    feature: add open assistant

--- src/api/open-ai/open-ai.controller.ts
+++ src/api/open-ai/open-ai.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from "@nestjs/common";
+import {Body, Controller, Get, Post} from "@nestjs/common";
 import {OpenAiService} from "./open-ai.service";
 
 @Controller('open-ai')
@@ -11,5 +11,25 @@ export class OpenAiController {
   @Post('request')
   async sendRequest(@Body() body: {message: string}) {
     return this.openAiService.sendRequest(body.message);
+  }
+
+  @Get('assistants')
+  async findAllAssistants() {
+    return this.openAiService.getAllAssistants();
+  }
+
+  @Post('message')
+  async sendMessage() {
+    return this.openAiService.createMessage()
+  }
+
+  @Post('run')
+  async run() {
+    return this.openAiService.createRun()
+  }
+
+  @Get('run')
+  async getRun(){
+    return this.openAiService.getRun()
   }
 }
--- src/api/open-ai/open-ai.service.ts
+++ src/api/open-ai/open-ai.service.ts
@@ -7,6 +7,8 @@ export class OpenAiService {
     apiKey: process.env.OPEN_AI_API_KEY ?? '',
   });
 
+  private readonly assistantId = 'asst_dPUdY6N2oV3Rkl5THgeRjCuD';
+
   async sendRequest(message: string) {
     const response = this.openAi.chat.completions.create({
       model: 'g-67c321a20f38819192ef9fcf90922551-test',
@@ -18,5 +20,36 @@ export class OpenAiService {
       ]
     })
     return response;
+  }
+
+  async getAllAssistants () {
+    return this.openAi.beta.assistants.list();
+  }
+
+  async createMessage(message: string) {
+    return this.openAi.beta.threads.create({
+      messages: [{
+        role: 'user',
+        content: 'Знайди викладача Рокового'
+      }]
+    })
+  }
+
+  async createRun () {
+    return this.openAi.beta.threads.createAndRun({
+      assistant_id: this.assistantId,
+      thread: {
+        messages: [
+          { role: "user", content: "Знайди викладача Рокового." },
+        ],
+      },
+    });
+  }
+
+  async getRun() {
+    return this.openAi.beta.threads.runs.retrieve(
+      'thread_nDoPBRwgjqwTH5FbkiRPLzEy',
+      'run_HCcWcbtV5QVDf6WqSxFoSalP',
+    )
   }
 }
