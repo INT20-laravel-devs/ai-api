bff5d3df1f667d568f641c19ffa3d50bed54a52e
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2025-03-02 03:18:40 +0200

    fix: add docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './api/auth/auth.module';
 import { UserModule } from './api/user/user.module';
 import { ChatModule } from './api/chat/chat.module';
 import { OpenAiModule } from './api/open-ai/open-ai.module';
+import { FiceAdvisorModule } from './api/ficeadvisor/ficeadvisor.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ChatModule, OpenAiModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    ChatModule,
+    OpenAiModule,
+    FiceAdvisorModule,
+  ],
 })
 export class AppModule {}
